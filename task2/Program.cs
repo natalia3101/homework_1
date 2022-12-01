3e3dfc5304d99f88ddf1a8820443f3872ca4b491
Natalia <dginni2009@gmail.com>
2022-12-01 17:21:46 +0300

    initial commit

---
+++ task2/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine("Enter 1st number");
+string numberStr1 = Console.ReadLine() ?? "";
+Console.WriteLine("Enter 2nd number");
+string numberStr2 = Console.ReadLine() ?? "";
+int number1 = Convert.ToInt32(numberStr1);
+int number2 = Convert.ToInt32(numberStr2);
+
+if (number1 > number2)
+{
+    Console.WriteLine($"max = {number1}");
+} else
+{
+    Console.WriteLine($"max = {number2}");
+}
---
+++ task4/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Enter 1st number");
+string numberStr1 = Console.ReadLine() ?? "";
+Console.WriteLine("Enter 2nd number");
+string numberStr2 = Console.ReadLine() ?? "";
+Console.WriteLine("Enter 3rd number");
+string numberStr3 = Console.ReadLine() ?? "";
+int number1 = Convert.ToInt32(numberStr1);
+int number2 = Convert.ToInt32(numberStr2);
+int number3 = Convert.ToInt32(numberStr3);
+
+int max = number1;
+if (number2 > max) max = number2;
+if (number3 > max) max = number3;
+Console.Write(max);
